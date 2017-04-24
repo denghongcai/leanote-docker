eda9e21f83bc053cf726d4b8b9903980a64ae751
Hongcai Deng <admin@dhchouse.com>
2017-04-25 01:45:39 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -4,3 +4,5 @@ conf=/usr/local/leanote/conf/app.conf
 sed -i "s/app.secret=.*$/app.secret=${HASH} #/" $conf
 sed -i "s/db.host=.*$/db.host=${DB_HOST}/" $conf
 sed -i "s/site.url=.*$/site.url=\${SITE_URL} /" $conf
+
+/usr/local/leanote/bin/run-app.sh
